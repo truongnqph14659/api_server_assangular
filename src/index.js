3343586800f3b42f036eff37baf91ebcec50faaf
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-10-17 22:22:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ server.use(auth)
 server.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin: https://assignmentangular.vercel.app','Access-Control-Allow-Methods: GET, POST, PUT')
 //     res.setHeader('Access-Control-Allow-Headers', '*')
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+   res.setHeader('Access-Control-Allow-Origin', 'https://assignmentangular.vercel.app')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', '*')
   res.setHeader('Access-Control-Allow-Credentials', true)
