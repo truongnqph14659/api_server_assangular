8e25d10ef977e75698f3ece54d04e3ad5fe54a5c
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-11-01 11:02:04 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ server.use(rules)
 server.use(auth)
 server.use((req, res, next) => {
    
-  res.setHeader('Authorization':Bearer prj_46tyyXPXmaUKpBnyc7mdVZm3NHkU)
+  res.setHeader('Authorization':'Bearer '+'prj_46tyyXPXmaUKpBnyc7mdVZm3NHkU')
 //  headers: {
 //             Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
 //         }
